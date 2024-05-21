0df7962c76c91e76517a5434fa0dbda6b77462dc
Shin HyeJi <liexil97@gmail.com>
2024-05-21 20:38:25 +0900

    complete

--- main.js
+++ main.js
@@ -1,31 +1,61 @@
+const { log } = require("console");
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// N = 나무의 수, M = 집에 가져갈 나무의 길이
+// [0] = 나무의 수, [1] = 필요 나무 길이
 let list = [];
-
-// 나무의 높이
+// 나무의 높이 배열
 let woods = [];
 
+let maxLength = 0;
+let sum = 0;
+
 function solution() {
-  console.log(list);
-  console.log(woods);
+  maxLength = woods[0];
+
+  // 절단기의 길이를 찾을 때까지 반복
+  for (let i = 0; i < woods[0]; i++) {
+    // 필요 길이가 충분하지 못하면, 길이를 줄여서 반복
+    if (sum < list[1]) {
+      maxLength = woods[0] - i;
+      cutter(maxLength);
+    } else {
+      console.log(maxLength);
+      return;
+    }
+  }
 }
 
-// 입력
+// woods 안에서 잘린 나무 길이의 합 반복
+function cutter(length) {
+  sum = 0;
+
+  for (let j = 0; j < list[0]; j++) {
+    sum += woods[j] - length;
+
+    // 필요 길이를 전부 구하면, 나머지 연산 없이 종료
+    if (sum >= list[1]) {
+      return;
+    }
+  }
+}
+
 rl.on("line", function (line) {
   if (!list.length) {
     list = line.split(" ").map((el) => Number(el));
   } else {
-    woods = line.split(" ").map((el) => Number(el));
+    // 내림차순 정렬
+    woods = line
+      .split(" ")
+      .map((el) => Number(el))
+      .sort((a, b) => b - a);
 
     return rl.close();
   }
 }).on("close", function () {
-  // 입력 종료
   solution();
   process.exit();
 });
